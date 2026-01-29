fc55b74f3517dc6df8022552384cc6fdff207c69
Tommy <tommymaz06@gmail.com>
2026-01-29 21:03:02 +0100

    menu tendina

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ AOS.init({ duration: 1000 });
 // Toggle menu mobile
 const btn = document.getElementById('menu-btn');
 const menu = document.getElementById('mobile-menu');
-
+const overlay = document.getElementById('overlay');
 
 btn.addEventListener('click', () => {
-    menu.classList.toggle('hidden');
+    menu.classList.toggle("right-0");
+    menu.classList.toggle("right-[-100%]");
+    overlay.classList.toggle("hidden");
 });
 
-
+overlay.addEventListener('click', () => {
+    menu.classList.add("right-[-100%]");
+    menu.classList.remove("right-0");
+    overlay.classList.add("hidden");
+});
