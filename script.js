bb4b1fcc3475ea84827e15fe87be37ae112a1c7a
Tommy <tommymaz06@gmail.com>
2026-01-29 21:45:41 +0100

    .

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Inizializza AOS
 AOS.init({ duration: 1000 });
 
-// Toggle menu mobile
 const btn = document.getElementById('menu-btn');
 const menu = document.getElementById('mobile-menu');
 const overlay = document.getElementById('overlay');
 const menuLinks = document.querySelectorAll('.menu-link');
 
+// Funzione per aprire/chiudere il menu
 function toggleMenu() {
     menu.classList.toggle("right-0");
     menu.classList.toggle("right-[-100%]");
