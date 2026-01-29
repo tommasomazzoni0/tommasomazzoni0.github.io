0e360d36e5c1019b1054fef2f7c34e0e62d8c44d
Tommy <tommymaz06@gmail.com>
2026-01-29 21:42:25 +0100

    a

--- script.js
+++ script.js
@@ -7,24 +7,23 @@ const menu = document.getElementById('mobile-menu');
 const overlay = document.getElementById('overlay');
 const menuLinks = document.querySelectorAll('.menu-link');
 
-// Funzione per aprire/chiudere il menu
 function toggleMenu() {
     menu.classList.toggle("right-0");
     menu.classList.toggle("right-[-100%]");
     overlay.classList.toggle("hidden");
 }
 
-// Click sul pulsante
+// Pulsante menu
 btn.addEventListener('click', toggleMenu);
 
-// Click sull’overlay
+// Overlay cliccabile per chiudere
 overlay.addEventListener('click', () => {
     menu.classList.add("right-[-100%]");
     menu.classList.remove("right-0");
     overlay.classList.add("hidden");
 });
 
-// Click su qualsiasi link del menu
+// Chiudi il menu cliccando un link
 menuLinks.forEach(link => {
     link.addEventListener('click', () => {
         menu.classList.add("right-[-100%]");
